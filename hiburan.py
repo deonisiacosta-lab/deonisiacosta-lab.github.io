f6029e9dd1b0b7d3538fdd4c332923a16349231d
deonisiacosta-lab <deonisiacosta@gmail.com>
2026-08-09 10:47:49 +0900

    Update hiburan.py

--- hiburan.py
+++ hiburan.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("✨ Supreza")
 
 # Hatama naran
-naran = st.text_input("Haruka Ita-nia Naran:", "Atou")
+naran = st.text_input("Haruka Ita-nia Naran:", "Atou","Deo")
 
 if naran.lower() == "atou":
     st.success("Parabéns Augusto Amaral! ! 🎉")
@@ -11,3 +11,4 @@ if naran.lower() == "atou":
     # Hatama foto husi folder (porezemplu naran file mak "agus_foto.jpg")
     # Asegura katak file foto ne'e iha folder ne'ebé hanesan ho kódigu app.py
     st.image("Augusto Amaral.jpg", caption="Besik ona atu troka helem oan ne'e ho sapeu lulik😁😍")
+      st.image("deo.jpg", caption="Besik ona atu troka helem oan ne'e ho sapeu lulik😁😍")
