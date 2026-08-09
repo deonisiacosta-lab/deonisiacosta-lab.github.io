0052f77841c007c4592bef79afa3f6ad0ee0b8f2
deonisiacosta-lab <deonisiacosta@gmail.com>
2026-08-09 10:51:09 +0900

    Update hiburan.py

--- hiburan.py
+++ hiburan.py
@@ -2,13 +2,19 @@ import streamlit as st
 
 st.title("✨ Supreza")
 
-# Hatama naran
-naran = st.text_input("Haruka Ita-nia Naran:", "Atou","Deo")
+# Hatama naran husi hasoru (user input)
+naran = st.text_input("Haruka Ita-nia Naran:", "")
 
-if naran.lower() == "atou":
-    st.success("Parabéns Augusto Amaral! ! 🎉")
-    
-    # Hatama foto husi folder (porezemplu naran file mak "agus_foto.jpg")
-    # Asegura katak file foto ne'e iha folder ne'ebé hanesan ho kódigu app.py
+# Halo konversaun ba letre ki'ik hotu atu fasil to'o hikas iha match
+naran_input = naran.strip().lower()
+
+if naran_input == "atou" or naran_input == "augusto amaral":
+    st.success("Parabéns Augusto Amaral! 🎉")
     st.image("Augusto Amaral.jpg", caption="Besik ona atu troka helem oan ne'e ho sapeu lulik😁😍")
-      st.image("deo.jpg", caption="Besik ona atu troka helem oan ne'e ho sapeu lulik😁😍")
+
+elif naran_input == "deo":
+    st.success("Parabéns Agus! 🌟")
+    st.image("Kassa.jpg", caption="Tidak ada komentar") # Troka tuir naran foto ne'ebé iha Ita-nia folder
+
+elif naran_input != "":
+    st.warning("Deskulpa, naran ne'e la iha rejisitu ba foto espesiál.")
